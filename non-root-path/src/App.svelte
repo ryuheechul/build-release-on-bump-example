203fb2b07e7af8df686ed21626f82636a1a4fbfa
Heechul Ryu <ryuhcii@gmail.com>
2021-06-25 09:51:51 +0100

    Revert "Remove user-email in release commit"
    This reverts commit b62d27a4d1d3fbbad3d6b01eb4813156e3d2d5f7.

--- non-root-path/src/App.svelte
+++ non-root-path/src/App.svelte
@@ -5,7 +5,7 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<p>version: 1.0.8</p>
+	<p>version: 1.0.7</p>
 </main>
 
 <style>
